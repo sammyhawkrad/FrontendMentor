c60c918cea7aaf5b36bae2308bdd4b21bc597f8d
Samuel Darkwah Manu <manusam14@yahoo.com>
2021-06-09 11:10:19 +0000

    FAC: Rotate accordion arrow on click

--- faq-accordion-card/script.js
+++ faq-accordion-card/script.js
@@ -1,13 +1,17 @@
 const faq = document.getElementsByClassName("question");
 
+
 for (let i = 0; i < faq.length; i++) {
     faq[i].addEventListener("click", function() {
         this.classList.toggle("active");
         const answer = this.nextElementSibling;
+        const arrow = this.firstElementChild;
         if (answer.style.display === "block") {
             answer.style.display = "none";
+            arrow.style.rotate = "none"
         } else {
             answer.style.display = "block";
+            arrow.style.rotate = "180deg"
         }        
     });
     
